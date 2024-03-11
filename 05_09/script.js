88b77cddcfbdad8f683ba59f046350d330b13abb
Sithembiso Sangweni <sithembiso72@gmail.com>
2024-03-11 13:53:06 +0200

    push

--- 05_09/script.js
+++ 05_09/script.js
@@ -19,7 +19,45 @@ const everydayPack = new Backpack(
 );
 
 const content = `
-  <article class="backpack" id="everyday">
+    <figure class="backpack__image">
+        <img src=${everydayPack.image} alt="" />
+      </figure>
+      <h1 class="backpack__name">${everydayPack.name}</h1>
+      <ul class="backpack__features">
+        <li class="packprop backpack__volume">Volume:<span> ${
+          everydayPack.volume
+        }l</span></li>
+        <li class="packprop backpack__color">Color:<span> ${
+          everydayPack.color
+        }</span></li>
+        <li class="backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
+        <li class="packprop backpack__pockets">Number of pockets:<span> ${
+          everydayPack.pocketNum
+        }</span></li>
+        <li class="packprop backpack__strap">Left strap length:<span> ${
+          everydayPack.strapLength.left
+        } inches</span></li>
+        <li class="packprop backpack__strap">Right strap length:<span> ${
+          everydayPack.strapLength.right
+        } inches</span></li>
+        <li class="packprop backpack__lid">Lid status:<span> ${
+          everydayPack.lidOpen
+        }</span></li>
+      </ul>
+    
+`;
+
+const main = document.getElementsByClassName(".maincontent");
+const newArticle = document.createElement("article");
+newArticle.classList.toggle("backpack");
+
+newArticle.setAttribute("id", "everyday")
+console.log(newArticle);
+newArticle.innerHTML = content;
+main.append(newArticle);
+
+/*
+const content = `
     <figure class="backpack__image">
       <img src=${everydayPack.image} alt="" />
     </figure>
@@ -45,9 +83,14 @@ const content = `
         everydayPack.lidOpen
       }</span></li>
     </ul>
-  </article>
 `;
 
-const main = document.querySelector(".maincontent");
 
-main.innerHTML = content;
+const main = document.querySelector(".maincontent");
+const newArticle = document.createElement("article");
+newArticle.classList.add("backpack")
+newArticle.setAttribute("id", "everyday");
+newArticle.innerHTML = content;
+main.append(newArticle);
+console.log(main);
+*/
--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,5 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+console.log(document.querySelector('article'));
+console.log(document.querySelectorAll('article'));
